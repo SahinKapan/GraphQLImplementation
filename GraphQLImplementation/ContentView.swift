a6f5fababf45b2c79fbbc69c6c42e18f59d1558b
SahinKapan <sahinkapan2635@gmail.com>
2025-04-22 16:03:22 +0300

    Initial Commit

---
+++ GraphQLImplementation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GraphQLImplementation
+//
+//  Created by Sahin Kapan on 22.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GraphQLImplementation/GraphQLImplementationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLImplementationApp.swift
+//  GraphQLImplementation
+//
+//  Created by Sahin Kapan on 22.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLImplementationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
